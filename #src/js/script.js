84201c3db7f93fe7dbf0664154a1847727dab634
Alex <tayrenov@gmail.com>
2021-03-09 19:31:42 +0300

    RequestText v.0.0.4  header + pop_up_telephone

--- #src/js/script.js
+++ #src/js/script.js
@@ -4,28 +4,38 @@ document.addEventListener('DOMContentLoaded', () => {
 
 /*Pop-up_telephone***/
 
+
+
+
 const header__pop_up_telephone = document.querySelector('.header__pop-up-telephone'),
       telephone = document.querySelector('.contacts__telephone'),
       header_close_btn = document.querySelector('#close_btn'),
       pop_up__wrapper = document.querySelector('.pop-up__wrapper');
 
-      telephone.addEventListener('click', function() {
+function toggle_Tel_popUp() {
+    if (document.documentElement.clientWidth > 1600) {
         header__pop_up_telephone.classList.toggle('header__pop-up__hide');
         header__pop_up_telephone.classList.toggle('header__pop-up__active');
         pop_up__wrapper.classList.toggle('active');
         pop_up__wrapper.classList.toggle('hide');
+        console.log(document.documentElement.clientWidth);
+    }
+}
+
+      telephone.addEventListener('click', function() {
+        toggle_Tel_popUp()
       });
 
       header_close_btn.addEventListener('click', function() {
-        header__pop_up_telephone.classList.toggle('header__pop-up__hide');
-        header__pop_up_telephone.classList.toggle('header__pop-up__active');
-        pop_up__wrapper.classList.toggle('active');
-        pop_up__wrapper.classList.toggle('hide');
+        toggle_Tel_popUp()
       });
+
 
       document.addEventListener('click', function(e) {
         let t=e.target;
-        console.log(t);
-      });
-
+  
+        if (t == pop_up__wrapper) {
+            toggle_Tel_popUp();
+        }
+      });      
 });
--- dist/js/script.js
+++ dist/js/script.js
@@ -4,16 +4,22 @@ document.addEventListener('DOMContentLoaded', () => {
 
 /*Pop-up_telephone***/
 
+
+
+
 const header__pop_up_telephone = document.querySelector('.header__pop-up-telephone'),
       telephone = document.querySelector('.contacts__telephone'),
       header_close_btn = document.querySelector('#close_btn'),
       pop_up__wrapper = document.querySelector('.pop-up__wrapper');
 
 function toggle_Tel_popUp() {
-    header__pop_up_telephone.classList.toggle('header__pop-up__hide');
-    header__pop_up_telephone.classList.toggle('header__pop-up__active');
-    pop_up__wrapper.classList.toggle('active');
-    pop_up__wrapper.classList.toggle('hide');
+    if (document.documentElement.clientWidth > 1600) {
+        header__pop_up_telephone.classList.toggle('header__pop-up__hide');
+        header__pop_up_telephone.classList.toggle('header__pop-up__active');
+        pop_up__wrapper.classList.toggle('active');
+        pop_up__wrapper.classList.toggle('hide');
+        console.log(document.documentElement.clientWidth);
+    }
 }
 
       telephone.addEventListener('click', function() {
@@ -27,10 +33,9 @@ function toggle_Tel_popUp() {
 
       document.addEventListener('click', function(e) {
         let t=e.target;
-        console.log(t);
+  
         if (t == pop_up__wrapper) {
             toggle_Tel_popUp();
         }
       });      
- 
 });
